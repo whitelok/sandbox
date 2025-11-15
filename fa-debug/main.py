b2a0c438cc5d0e42f2d92bff3ae53e265b3ff9d1
骆兆楷 <luozhaokai@xiaohongshu.com>
2025-11-15 10:12:09 +0800

    adding code by karlluo

--- fa-debug/main.py
+++ fa-debug/main.py
@@ -18,17 +18,17 @@ NUM_HEADS = 16
 HEAD_DIM = 64
 DEPTH = 24
 PATCH_SIZE = 16
-SEQ_LEN = (IMG_SIZE // PATCH_SIZE)**2 # 196
+SEQ_LEN = (IMG_SIZE // PATCH_SIZE)**2  # 196
+
 
 class MyAttentionBlock(nn.Module):
-    def __init__(
-            self,
-            attn_fn,
-            format = None,
-            dim: int = 768,
-            num_heads: int = 12,
-            **kwargs
-    ) -> None:
+
+    def __init__(self,
+                 attn_fn,
+                 format=None,
+                 dim: int = 768,
+                 num_heads: int = 12,
+                 **kwargs) -> None:
         super().__init__()
         self.attn_fn = attn_fn
         self.num_heads = num_heads
@@ -42,12 +42,12 @@ class MyAttentionBlock(nn.Module):
             hidden_features=dim * 4,
         )
         permute = (2, 0, 3, 1, 4)
-        self.permute_attn = functools.partial(torch.transpose,dim0=1,dim1=2)
+        self.permute_attn = functools.partial(torch.transpose, dim0=1, dim1=2)
 
         if format == 'bshd':
             permute = (2, 0, 1, 3, 4)
             self.permute_attn = nn.Identity()
-        self.permute_qkv = functools.partial(torch.permute,dims=permute)
+        self.permute_qkv = functools.partial(torch.permute, dims=permute)
 
     def forward(self, x_in: torch.Tensor) -> torch.Tensor:
         x = self.norm1(x_in)
@@ -64,3 +64,14 @@ class MyAttentionBlock(nn.Module):
         x = x + x_in
         x = x + self.mlp(self.norm2(x))
         return x
+
+
+class FakeDataset(Dataset):
+
+    def __len__(self):
+        return 1000000
+
+    def __getitem__(self, index):
+        rand_image = torch.randn([3, IMG_SIZE, IMG_SIZE], dtype=torch.float32)
+        label = torch.tensor(data=index % 1000, dtype=torch.int64)
+        return rand_image, label
